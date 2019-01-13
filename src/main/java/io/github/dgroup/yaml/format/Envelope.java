c7f3dec8ea43dd14505dbcff4329882a88299edf
dgroup <yurii.dubinka@gmail.com>
2019-01-13 16:58:18 +0530

    #24: Add Envelope for 'io.github.dgroup.yaml.Format'

--- src/main/java/io/github/dgroup/yaml/format/Envelope.java
+++ src/main/java/io/github/dgroup/yaml/format/Envelope.java
@@ -21,26 +21,53 @@
  * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
  * OR OTHER DEALINGS IN THE SOFTWARE.
  */
-package io.github.dgroup.yaml;
+package io.github.dgroup.yaml.format;
+
+import io.github.dgroup.yaml.Format;
+import io.github.dgroup.yaml.YamlFormatException;
+import org.cactoos.Scalar;
 
 /**
- * Represents YAML file as an object.
+ * Envelope of {@link Format}.
  *
- * @since 0.1.0
- * @todo #/DEV Rename to Yaml and extends Scalar. It shouldn't be focused on top
- *  on snakeyaml only, thus the implementation YamlOf should support other
- *  frameworks through the passing of org.cactoos.Scalar or org.cactoos.Func.
+ * @param <T> The type of object to be built from YAML file.
+ * @since 0.3.0
  */
-public interface YamlFile {
+public class Envelope<T> implements Format<T> {
 
     /**
-     * Represents *.yaml file as object.
-     * @param version The expected version of the file.
-     * @param type The type of target object to be built from *.yaml file
-     * @param <T> The type of target object.
-     * @return The object to be built from the *.yaml file.
-     * @throws YamlFormatException In case if YAML file can't be read or the
-     *  file has unsupported version/format.
+     * The version of current YAML format.
      */
-    <T> T read(Object version, Class<T> type) throws YamlFormatException;
+    private final String vrsn;
+
+    /**
+     * The object to be build from YAML file.
+     */
+    private final Scalar<T> frmt;
+
+    /**
+     * Ctor.
+     * @param vrsn The version of current YAML format.
+     * @param frmt The object to be built from YAML file.
+     */
+    public Envelope(final String vrsn, final Scalar<T> frmt) {
+        this.vrsn = vrsn;
+        this.frmt = frmt;
+    }
+
+    @Override
+    public final String version() {
+        return this.vrsn;
+    }
+
+    @Override
+    @SuppressWarnings("PMD.AvoidCatchingGenericException")
+    public final T value() throws YamlFormatException {
+        try {
+            return this.frmt.value();
+            // @checkstyle IllegalCatchCheck (3 lines)
+        } catch (final Exception exp) {
+            throw new YamlFormatException(exp);
+        }
+    }
 }
